4af5ef8a0c2b403de48563ec5dc7738fd711acfb
Denis Pozder <denispozder6@gmail.comm>
2024-04-05 03:39:10 +0200

    Saving Changes

--- server/server.js
+++ server/server.js
@@ -14,6 +14,13 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use(cors({
+    origin: 'https://denivito-ten.vercel.app/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+}))
+
 // Connect DB
 connectDB()
 
